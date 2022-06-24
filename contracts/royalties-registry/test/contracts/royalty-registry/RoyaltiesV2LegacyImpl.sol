54d5bfbbdce3072db0dcc9d8f1056d5e159416c0
Erik <erik.sbstn@gmail.com>
2022-06-24 16:45:48 +0200

    ya ni lo se

--- contracts/royalties-registry/test/contracts/royalty-registry/RoyaltiesV2LegacyImpl.sol
+++ contracts/royalties-registry/test/contracts/royalty-registry/RoyaltiesV2LegacyImpl.sol
@@ -6,14 +6,21 @@ pragma abicoder v2;
 import "../../../contracts/providers/RoyaltyV2Legacy.sol";
 
 contract RoyaltiesV2LegacyImpl is RoyaltyV2Legacy {
+    mapping(uint256 => LibPart.Part[]) internal royalties;
 
-    mapping (uint256 => LibPart.Part[]) internal royalties;
-
-    function _saveRoyalties(uint256 id, LibPart.Part[] memory _royalties) public {
+    function _saveRoyalties(uint256 id, LibPart.Part[] memory _royalties)
+        public
+    {
         uint256 totalValue;
-        for (uint i = 0; i < _royalties.length; i++) {
-            require(_royalties[i].account != address(0x0), "Recipient should be present");
-            require(_royalties[i].value != 0, "Royalty value should be positive");
+        for (uint256 i = 0; i < _royalties.length; i++) {
+            require(
+                _royalties[i].account != address(0x0),
+                "Recipient should be present"
+            );
+            require(
+                _royalties[i].value != 0,
+                "Royalty value should be positive"
+            );
             totalValue += _royalties[i].value;
             royalties[id].push(_royalties[i]);
         }
@@ -21,20 +28,31 @@ contract RoyaltiesV2LegacyImpl is RoyaltyV2Legacy {
         _onRoyaltiesSet(id, _royalties);
     }
 
-    function _updateAccount(uint256 _id, address _from, address _to) internal {
-        uint length = royalties[_id].length;
-        for(uint i = 0; i < length; i++) {
+    function _updateAccount(
+        uint256 _id,
+        address _from,
+        address _to
+    ) internal {
+        uint256 length = royalties[_id].length;
+        for (uint256 i = 0; i < length; i++) {
             if (royalties[_id][i].account == _from) {
-                royalties[_id][i].account = address(uint160(_to));
+                royalties[_id][i].account = payable(_to);
             }
         }
     }
 
-    function getRoyalties(uint256 id) override external view returns (LibPart.Part[] memory) {
+    function getRoyalties(uint256 id)
+        external
+        view
+        override
+        returns (LibPart.Part[] memory)
+    {
         return royalties[id];
     }
 
-    function _onRoyaltiesSet(uint256 id, LibPart.Part[] memory _royalties) internal {
+    function _onRoyaltiesSet(uint256 id, LibPart.Part[] memory _royalties)
+        internal
+    {
         emit RoyaltiesSet(id, _royalties);
     }
 }
